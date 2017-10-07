53522a61e282366be0f489e5a6554e879b485c7e
Juanma <aljumaro@gmail.com>
2017-10-07 19:01:06 +0200

    Classic propertyplaceholderconfigurer

---
+++ src/main/java/classic/Application.java
@@ -0,0 +1,16 @@
+package classic;
+
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		new ClassPathXmlApplicationContext("classic-context.xml");
+	}
+
+	public void setConfigurationProjectName(String projectName) {
+		LogFactory.getLog(getClass()).info("the configuration project name is " + projectName);
+	}
+}
