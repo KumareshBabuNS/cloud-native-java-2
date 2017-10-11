4e32cb811a6043666944adaea66339f6e22a97a5
jalvarro <jalvarro@LCG-2XWNVF2.usersad.everis.int>
2017-10-11 18:58:58 +0200

    Config client configuration

--- src/main/java/com/example/demo/CloudFoundryTestApplication.java
+++ src/main/java/com/example/demo/CloudFoundryTestApplication.java
@@ -1,14 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@ComponentScan
 @SpringBootApplication
 public class CloudFoundryTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudFoundryTestApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${configuration.projectName:Config default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
 	}
 }
